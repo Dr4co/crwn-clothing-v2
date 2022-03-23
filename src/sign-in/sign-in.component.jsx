95f1f83bf5348a2f5ab6c99885b2712ab216cc0e
Ting-Hey Chau <tinghey.chau@gmail.com>
2022-03-23 16:46:48 +0100

    Remove example Google redirect provider

--- src/sign-in/sign-in.component.jsx
+++ src/sign-in/sign-in.component.jsx
@@ -1,22 +1,9 @@
-import {useEffect} from 'react'
-import {getRedirectResult} from 'firebase/auth'
 import { 
-    auth,
     signInWithGooglePopup,
     createUserDocumentFromAuth,
-    signInWithGoogleRedirect
 } from '../utils/firebase/firebase.utils'
 
-
-
 const SignIn = () => {
-    useEffect(async () => {
-            const response = await getRedirectResult(auth);
-            if(response) {
-                const userDocRef = await createUserDocumentFromAuth(response.user);
-            }
-    }, []);
-
     const logGoogleUser = async () => {
         const {user} = await signInWithGooglePopup();
         const userDocRef = createUserDocumentFromAuth(user);
@@ -28,10 +15,6 @@ const SignIn = () => {
             <button onClick={logGoogleUser}>
                 Sign in with Google Popup
             </button>
-            <button onClick={signInWithGoogleRedirect}>
-                Sign in with Google Redirect
-            </button>
-
         </div>
     )
 }
